08cb21cc5f4c42e25234582f87316bb3956e3721
sh4d0wg0d <shad0w_g0d@hotmail.com>
2024-09-01 09:47:32 -0700

    Update TestController.java
     /Data will show: "Data message from AWS Ecs"
    /Message will show:  "Message from AWS Ecs"

--- src/main/java/com/example/springbootawsdeploy/TestController.java
+++ src/main/java/com/example/springbootawsdeploy/TestController.java
@@ -9,11 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping("/data")
     public String getData() {
-        return "First message from AWS Ecs";
+        return "Data message from AWS Ecs";
     }
 
     @GetMapping("/message")
     public String getMessage() {
-        return "Second message from AWS Ecs";
+        return "Message from AWS Ecs";
     }
 }
